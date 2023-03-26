872ee7e6d9f10b3c3fc15fee263c594befc97204
Marcin <marcin.szturminski@gmail.com>
2023-03-26 19:50:56 +0200

    added work section, added turn off scroll when using nav menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,40 @@
 const hamburgerBtn = document.querySelector('.hamburger')
 const navMobile = document.querySelector('.nav__mobile-list')
 const introduction = document.querySelector('.introduction')
-// console.log(testBtn)
+const body = document.querySelector('body')
+
+console.log(body)
 
 const showNav = () => {
 	navMobile.classList.toggle('nav__mobile-list-active')
 	hamburgerBtn.classList.toggle('is-active')
 	if (introduction.style.filter === 'blur(4px)') {
 		introduction.style.filter = ''
+		body.style.overflow = ''
 	} else {
 		introduction.style.filter = 'blur(4px)'
+		body.style.overflow = 'hidden'
 	}
 }
 
 hamburgerBtn.addEventListener('click', showNav)
+
+// ------------------------------------------------
+// accordion
+
+const workIcon = document.querySelector('.work__item-icon')
+const workDescription = document.querySelector('.work__item-description')
+
+console.log(workIcon)
+console.log(workDescription)
+
+const showDescription = () => {
+	workDescription.classList.toggle('active')
+	if (workIcon.style.transform === 'rotate(180deg)') {
+		workIcon.style.transform = ''
+	} else {
+		workIcon.style.transform = 'rotate(180deg)'
+	}
+}
+
+workIcon.addEventListener('click', showDescription)
